be6be30f2a7d25d15632c7e7702102a424ad91b5
KV Bharat Bhushan <56403997+KVBharatBhushan@users.noreply.github.com>
2020-05-06 10:10:01 +0530

    update
    update

--- LibScan.ps1
+++ LibScan.ps1
@@ -11,8 +11,8 @@ $CSVPath = "D:\LibraryDocumentsInventory.csv"
 
 
 #Config Login Details with Password Protection
-$User = "O365admin@crossshare.net"
-$PWord = ConvertTo-SecureString -String "Cambay#0987" -AsPlainText -Force
+$User = "O365admin@sampleshare.net"
+$PWord = ConvertTo-SecureString -String "*******" -AsPlainText -Force
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
 
 
