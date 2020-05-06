1f469d4bee84f56f039ceba2cd53a75bb9a2968a
KV Bharat Bhushan <56403997+KVBharatBhushan@users.noreply.github.com>
2020-05-06 10:09:20 +0530

    update
    update

--- LibRemoveFiles.ps1
+++ LibRemoveFiles.ps1
@@ -1,6 +1,6 @@
 ﻿#Config Login Details with Password Protection
-$User = "O365admin@crossshare.net"
-$PWord = ConvertTo-SecureString -String "Cambay#0987" -AsPlainText -Force
+$User = "O365admin@sampleshare.net"
+$PWord = ConvertTo-SecureString -String "*********" -AsPlainText -Force
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
 
 $SourceSiteURL="https://crosssharenet.sharepoint.com/sites/classictest"
